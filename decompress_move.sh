2a8fa7cc118ae5a688820a07f54813c340d500d9
TJ Zimmerman <tj@tjzimmerman.com>
2017-03-28 17:06:40 -0400

    Program near completion. Has error handling and supports user input. Buggy cp post exit when replacing files?

--- decompress_move.sh
+++ decompress_move.sh
@@ -1,34 +1,63 @@
 #!/bin/bash
-###############################################################
-#Program takes input from the 'incoming' directory as gzipped #
-#files, decompresses them, moves them to the 'data' directory #
-#and deletes the original archive. Files that have been moved #
-#to the data directory have been placed in a subdirectory with#
-#the same name as the original archive which maintains the    #
-#following naming schema: DATA_YYYMMDD_hhmm.csv.gz            #
-###############################################################
+echo
+echo "******************************"
+echo "*****CSV Dataset Migrator*****"
+echo "******************************"
+echo
 
-#Check to see how many datasets are in 'incoming'. Ignore errors 
-#because ls throws a non-zero exit code when your glob doesn't 
-#return any matches.
+
+#Check to see how many datasets are in 'incoming'. Ignore errors because ls throws a non-zero exit code when your glob doesn't return any matches.
 numArchives=$((ls incoming/*.csv.gz | wc -l) 2> /dev/null)
-
-#If there are no incoming datasets, exit with a status code.
 if [[ $numArchives -lt 1 ]]
 then
-	echo "$(date) - There are no incoming archives to parse."
+	#If there are no incoming datasets, exit with a status code.
+	echo "There are no incoming archives to parse. Exiting Now..."
 	exit 1 
 else
-	echo "$(date) - There are $numArchives archives to parse."
+	echo "There are $numArchives archives to parse. Parsing Now..." && echo
 fi
 
-echo && echo "$(date) - Parsing files now..."
 
-#For each archive in 'incoming', create a folder within 'data'
-#with the same base name. Trim off the unncessary parts including 
-#'DATA_' and '.csv.gz'.
+
+#For each archive in `incoming`, extract it and place it in `data`. #Trim off unneccessary information.
 for i in $(ls incoming/*.csv.gz)
 do
-	dirName=$(echo $i | sed 's@.*/@@' | sed 's/\..*$//')
-	mkdir data/$dirName
+	echo -n .
+	fileName=$(echo $i | sed 's@.*/@@' | sed 's/\.gz.*$//')
+
+
+	if [ -f "data/$fileName" ]
+	then
+		echo && echo
+		while true
+		do
+			read -p "$fileName already exists in 'data'. Would you like to Skip/Replace/Append the file or Exit the program? (s/r/a/e) " srae
+			case $srae in
+				[Ss]* ) echo "$(date) - [WARN] - A file named $fileName already exists within ./data/. This CSV dataset has been skipped." >> output.log 2>&1; break;;
+				[Rr]* ) echo "$(date) - [WARN] - A file named $fileName already exists within ./data/. The old CSV dataset has been replaced with the new CSV dataset." >> output.log 2>&1; gunzip -c $i > data/$fileName 2>&1; break;;
+				[Aa]* ) echo "$(date) - [WARN] - A file named $fileName already exists within ./data/. The new CSV dataset has had its name appended to preserve both files." >> output.log 2>&1; gunzip -c $i > data/$fileName 2>&1; break;;
+				[Ee]* ) echo "$(date) - [ERROR] - CSV Dataset Migrator was interrupted before completion." >> output.log 2>&1; break 2;;
+				* ) echo "Please answer with (Ss/Rr/Aa/Ee)."
+			esac	
+		done
+	else
+	
+		echo "$(date) - [INFO] - Extracting $i to data..." >> output.log 2>&1
+		gunzip -c $i > data/$fileName 2>&1
+		echo "$(date) - [INFO] - Cleaning up old archive..." >> output.log 2>&1
+		rm $i
+	fi
+done
+
+
+#Ask the user if they would like to see the log
+echo && echo 
+while true
+do
+	read -p "CSV Dataset Migrator has finished. Would you like to see the log? (y/n) " yn
+	case $yn in
+		[Yy]* ) less output.log; exit;;
+		[Nn]* ) echo; exit;;
+		* ) echo "Please answer with (Yy/Nn)."
+	esac
 done
