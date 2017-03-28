54e37437881dbd6706e2864765de66836bec96a0
TJ Zimmerman <tj@tjzimmerman.com>
2017-03-28 13:28:49 -0400

    Added conditional check of # of archives with basic error handling and logging.

--- decompress_move.sh
+++ decompress_move.sh
@@ -7,3 +7,17 @@
 #the same name as the original archive which maintains the    #
 #following naming schema: DATA_YYYMMDD_hhmm.csv.gz            #
 ###############################################################
+
+#Check to see how many datasets are in 'incoming'. Ignore errors because ls throws a non-zero exit code when your glob doesn't return any matches.
+numArchives=$((ls incoming/*.csv.gz | wc -l) 2> /dev/null)
+
+#If there are no incoming datasets, exit with a status code.
+if [[ $numArchives -lt 1 ]]
+then
+	echo "$(date) - There are no incoming archives to parse."
+	exit 1 
+else
+	echo "$(date) - There are $numArchives archives to parse."
+fi
+
+for
